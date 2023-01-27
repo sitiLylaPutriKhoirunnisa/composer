bad35d89630b389ed083096f3a35251f8557a03c
sitiLylaPutriKhoirunnisa <110007980+sitiLylaPutriKhoirunnisa@users.noreply.github.com>
2023-01-27 09:20:40 +0700

    Add files via upload

---
+++ dompdf.php
@@ -0,0 +1,38 @@
+<?php 
+
+require 'vendor/autoload.php';
+
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+$faker = Faker\Factory::create();
+$dompdf = new Dompdf();
+
+$nama = $faker->name();
+$keterangan = $faker->text();
+
+$dompdf->loadHtml("
+    <table border='1'>
+        <tr>
+            <td>nama</td>
+            <td>keterangan</td>
+        </tr>
+        <tr>
+            <td>".$nama
+            ."</td>
+            <td>".$keterangan
+            ."</td>
+            </tr>
+    </table>
+
+");
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
